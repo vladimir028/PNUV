710801c3fc3e56e39d2f62fc003de55d1068bd25
vladimir028 <107487074+vladimir028@users.noreply.github.com>
2025-05-25 17:19:08 +0200

    initial project structure

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+
+import MoodTracker from "./pages/mood-tracker";
+import DataHbsc from "./pages/data-hbsc";
+import Navigation from "./components/navigation";
+import Home from "./pages/home";
+import Footer from "./components/footer";
+
+const App = () => {
+  return (
+      <Router>
+        <Navigation />
+          <div className="container mt-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/mood-tracker" element={<MoodTracker />} />
+              <Route path="/hbsc-data" element={<DataHbsc />} />
+            </Routes>
+          </div>
+        <Footer />
+      </Router>
+  );
+};
+
+export default App;
