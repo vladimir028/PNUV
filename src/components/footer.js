0f99b5e9b190539c01321240ba06ac106b0176a0
vladimir028 <107487074+vladimir028@users.noreply.github.com>
2025-05-25 17:20:42 +0200

    implement footer

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer
+            style={{
+                marginTop: 'auto',
+                padding: '20px 0',
+                textAlign: 'center',
+                backgroundColor: '#00B9F2',
+                color: '#fff',
+                fontWeight: '600',
+                boxShadow: '0 -4px 10px rgba(0, 185, 242, 0.4)',
+            }}
+        >
+            <p>
+                © 2025 Сите права задржани | Составено од <strong>Владимир Грнчаровски (211028)</strong>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
